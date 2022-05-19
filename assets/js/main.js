6053f9d61eeefbf241ed62bdb95e9ef6e4cd7f34
Jarno Ruokamo <jarno.ruokamo@gmail.com>
2022-05-19 22:47:38 +0300

    Turha js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,8 @@
-/*! npm.im/supports-webp 2.0.1 */
-var supportsWebP = (function () {
-	'use strict';
-	
-	var index = new Promise(function (resolve) {
-		var image = new Image();
-		image.onerror = function () { return resolve(false); };
-		image.onload = function () { return resolve(image.width === 1); };
-		image.src = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=';
-	}).catch(function () { return false; });
-	
-	return index;
-	
-}());
-
-supportsWebP.then(supported => {
-	if (!supported) {
-		document.body.classList.add("no-webp");
-	}
-});
-
 var suomeksi = document.getElementById('suomeksi');
 
 ['click', 'touchend'].forEach(function(e) {
 	suomeksi.addEventListener(e, function() {
-		document.querySelectorAll('[data-fi]').forEach(function(el) {
-			el.innerHTML = el.dataset.fi;
-		});
+		changeLanguage('fi');
 	});
 });
 
@@ -33,8 +10,14 @@ var enkuksi = document.getElementById('enkuksi');
 
 ['click', 'touchend'].forEach(function(e) {
 	enkuksi.addEventListener(e, function() {
-		document.querySelectorAll('[data-en]').forEach(function(el) {
-			el.innerHTML = el.dataset.en;
-		});
+		changeLanguage('en');
 	});
 });
+
+function changeLanguage(lang) {
+	document.querySelectorAll('[data-' + lang + ']').forEach(function(el) {
+		el.innerHTML = el.dataset[lang];
+	});
+}
+
+if (navigator.language != 'fi') changeLanguage('en');
